093f53ad11f3d64d7d05dd319410e121ab989a90
Priyank Parashar <paras20xx@gmail.com>
2019-05-03 14:22:05 +0400

    Added support for custom redirect status

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # express-redirect-to-https
-Express middleware for redirecting "http" requests to "https" url path
+Express middleware for redirecting `http` requests to `https` url path.
+
+## Usage
+
+```js
+var express = require('express');
+var redirectToHttps = require('express-redirect-to-https');
+
+var app = express();
+```
+
+```js
+app.use(redirectToHttps());
+
+// OR
+
+app.use(redirectToHttps({
+    httpPort: 3000,
+    httpsPort: 4000
+}));
+
+// OR
+
+app.use(redirectToHttps({
+    httpPort: 3000,
+    httpsPort: 4000,
+    redirectStatus: 307 // Default redirect status is 301
+}));
+```
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var redirectToHttps = function (options) {
     options = options || {};
+    var httpPort = options.httpPort,
+        httpsPort = options.httpsPort,
+        redirectStatus = options.redirectStatus || 301;
     return function (req, res, next) {
         if (req.protocol === 'http') {
             var redirectToUrl;
             // http://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express/10185427#10185427
-            if (options.httpPort || options.httpsPort) {
-                redirectToUrl = 'https://' + req.get('host').replace(options.httpPort + '', options.httpsPort + '') + req.originalUrl;
+            if (httpPort || httpsPort) {
+                redirectToUrl = 'https://' + req.get('host').replace(httpPort + '', httpsPort + '') + req.originalUrl;
             } else {
                 redirectToUrl = 'https://' + req.get('host') + req.originalUrl;
             }
-            return res.redirect(301, redirectToUrl);
+            console.log(redirectStatus);
+            return res.redirect(redirectStatus, redirectToUrl);
         }
         next();
     };
