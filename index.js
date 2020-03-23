310b1ff8f87e65f5ecf1afed6501d36410d6142d
Priyank Parashar <paras20xx@gmail.com>
2020-03-23 05:55:59 +0400

    Improved error logging

--- index.js
+++ index.js
@@ -5,14 +5,33 @@ var redirectToHttps = function (options) {
         redirectStatus = options.redirectStatus || 301;
     return function (req, res, next) {
         if (req.protocol === 'http') {
-            var redirectToUrl;
-            // http://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express/10185427#10185427
-            if (httpPort || httpsPort) {
-                redirectToUrl = 'https://' + req.get('host').replace(httpPort + '', httpsPort + '') + req.originalUrl;
-            } else {
-                redirectToUrl = 'https://' + req.get('host') + req.originalUrl;
+            try {
+                var redirectToUrl;
+                // http://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express/10185427#10185427
+                if (httpPort || httpsPort) {
+                    redirectToUrl = 'https://' + req.get('host').replace(httpPort + '', httpsPort + '') + req.originalUrl;
+                } else {
+                    redirectToUrl = 'https://' + req.get('host') + req.originalUrl;
+                }
+                return res.redirect(redirectStatus, redirectToUrl);
+            } catch (e) {
+                console.error(
+                    `Error: Error caught in express-redirect-to-https.` +
+                    ` req.headers = ${req.headers} ;` +
+                    ` req.host = ${req.host} ;` +
+                    ` req.hostname = ${req.hostname} ;` +
+                    ` req.method = ${req.method} ;` +
+                    ` req.originalUrl = ${req.originalUrl} ;` +
+                    ` req.path = ${req.path} ;` +
+                    ` req.protocol = ${req.protocol} ;` +
+                    ` req.subdomains = ${req.subdomains} ;` +
+                    ` req.url = ${req.url} ;` +
+                    ` req._parsedUrl = ${req._parsedUrl} ;`
+                );
+                console.error(e);
+
+                throw e;
             }
-            return res.redirect(redirectStatus, redirectToUrl);
         }
         next();
     };
