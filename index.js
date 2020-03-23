ab4ebb29cf6588999f13ffcaa1489b28874fdc8f
Priyank Parashar <paras20xx@gmail.com>
2020-03-23 06:08:45 +0400

    Minor changes related to error handling

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var redirectToHttps = function (options) {
         httpsPort = options.httpsPort,
         redirectStatus = options.redirectStatus || 307;
     return function (req, res, next) {
-        if (req.protocol === 'http') {
-            try {
+        try {
+            if (req.protocol === 'http') {
                 var redirectToUrl;
                 // http://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express/10185427#10185427
                 if (httpPort || httpsPort) {
@@ -14,26 +14,26 @@ var redirectToHttps = function (options) {
                     redirectToUrl = 'https://' + req.get('host') + req.originalUrl;
                 }
                 return res.redirect(redirectStatus, redirectToUrl);
-            } catch (e) {
-                console.error(
-                    `Error: Error caught in express-redirect-to-https.` +
-                    ` req.headers = ${req.headers} ;` +
-                    ` req.host = ${req.host} ;` +
-                    ` req.hostname = ${req.hostname} ;` +
-                    ` req.method = ${req.method} ;` +
-                    ` req.originalUrl = ${req.originalUrl} ;` +
-                    ` req.path = ${req.path} ;` +
-                    ` req.protocol = ${req.protocol} ;` +
-                    ` req.subdomains = ${req.subdomains} ;` +
-                    ` req.url = ${req.url} ;` +
-                    ` req._parsedUrl = ${req._parsedUrl} ;`
-                );
-                console.error(e);
-
-                throw e;
             }
+            next();
+        } catch (e) {
+            console.error(e);
+            console.error(
+                `Error: Error caught in express-redirect-to-https.` +
+                ` req.headers = ${req.headers} ;` +
+                ` req.host = ${req.host} ;` +
+                ` req.hostname = ${req.hostname} ;` +
+                ` req.method = ${req.method} ;` +
+                ` req.originalUrl = ${req.originalUrl} ;` +
+                ` req.path = ${req.path} ;` +
+                ` req.protocol = ${req.protocol} ;` +
+                ` req.subdomains = ${req.subdomains} ;` +
+                ` req.url = ${req.url} ;` +
+                ` req._parsedUrl = ${req._parsedUrl} ;`
+            );
+
+            throw e;
         }
-        next();
     };
 };
 module.exports = redirectToHttps;
