b49b65a6b0ebbfc6342e234a60c6afd17cad0066
Priyank Parashar <paras20xx@gmail.com>
2020-03-24 04:27:50 +0400

    Improved robustness

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 var redirectToHttps = function (options) {
     options = options || {};
     var httpPort = options.httpPort,
-        httpsPort = options.httpsPort,
-        redirectStatus = options.redirectStatus || 307;
+        httpsPort = options.httpsPort;
+
+    var redirectStatus = options.redirectStatus;
+    if (typeof redirectStatus === 'number' && redirectStatus >= 300 && redirectStatus <= 399) {
+        // do nothing
+    } else {
+        redirectStatus = 307; // Temporary redirect
+    }
+
     return function (req, res, next) {
         try {
             if (req.protocol === 'http') {
-                var redirectToUrl;
-                // http://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express/10185427#10185427
-                if (httpPort || httpsPort) {
-                    redirectToUrl = 'https://' + req.get('host').replace(httpPort + '', httpsPort + '') + req.originalUrl;
-                } else {
-                    redirectToUrl = 'https://' + req.get('host') + req.originalUrl;
+                if (typeof req.get('host') === 'string') {
+                    var redirectToUrl;
+                    // http://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express/10185427#10185427
+                    if (httpPort || httpsPort) {
+                        redirectToUrl = 'https://' + req.get('host').replace(httpPort + '', httpsPort + '') + req.originalUrl;
+                    } else {
+                        redirectToUrl = 'https://' + req.get('host') + req.originalUrl;
+                    }
+                    return res.redirect(redirectStatus, redirectToUrl);
+                } else { // To reach this "else" condition, try something like: curl -H "host:" 127.0.0.1
+                    return res.status(400).send('400 Bad Request - Please access the "https://" version of this website.\n');
                 }
-                return res.redirect(redirectStatus, redirectToUrl);
             }
             next();
         } catch (e) {
@@ -21,6 +32,7 @@ var redirectToHttps = function (options) {
             console.error(
                 `Error: Error caught in express-redirect-to-https.` +
                 ` req.headers = ${JSON.stringify(req.headers)} ;` +
+                ` req.get('host') = ${req.get('host')} ;` +
                 ` req.host = ${req.host} ;` +
                 ` req.hostname = ${req.hostname} ;` +
                 ` req.method = ${req.method} ;` +
